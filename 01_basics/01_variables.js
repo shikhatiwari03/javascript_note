8f7310daef83730768d3866b0c2a5706e1a13034
shikha33 <167459775+shikhatiwari03@users.noreply.github.com>
2024-04-19 09:31:00 -0400

    initial commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId = 12345
+let accountemail = "shk@google.com"
+var accountpassword ="56789"
+accountcity = "jaipur"
+
+//accountId=5// not allowd because const cannot be changed
+console.log(accountId);
+
+console.table([accountId, accountemail, accountpassword, accountcity])
